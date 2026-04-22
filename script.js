fc54e50778db4f8d21f8b11c587bd14e60ae3fc3
Skatecrete <skatecrete@gmail.com>
2026-04-22 17:39:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,7 @@ let currentDebutData = null;
 
 // Apps Script URL
 // Try this proxy instead
-const SCRIPT_URL = 'https://api.allorigins.win/raw?url=' + encodeURIComponent('https://script.google.com/macros/s/AKfycbx6i6Yn7ezXqwJKgZF3Mbq_MbgNeb4mQ8weT0Qipu0c9ASFRVK6l-HIdH83xFbJOeI4/exec');
-// Pricing cache
+const SCRIPT_URL = 'https://cors-anywhere.herokuapp.com/' + 'https://script.google.com/macros/s/AKfycbx6i6Yn7ezXqwJKgZF3Mbq_MbgNeb4mQ8weT0Qipu0c9ASFRVK6l-HIdH83xFbJOeI4/exec';// Pricing cache
 let pricingCache = {};
 let coinPrices = { 5600: 24, 15500: 45, 31000: 85 };
 
