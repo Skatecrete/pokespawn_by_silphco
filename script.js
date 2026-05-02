e3859df00138de3d190ddaa50a2ba3009a604ded
Skatecrete <skatecrete@gmail.com>
2026-05-02 18:39:44 -0400

    Refactor modal display by removing gallery item logic
    Removed the gallery item generation logic from the modal display function.

--- script.js
+++ script.js
@@ -1715,21 +1715,6 @@ function showGalleryView() {
     
     modal.style.display = 'flex';
 }
-    
-    var grid = document.getElementById('galleryGrid');
-    var html = '';
-    for (var i = 0; i < allInfographicsList.length; i++) {
-        var item = allInfographicsList[i];
-        html += `
-            <div class="gallery-item" onclick="openImageViewer(${i})">
-                <img src="${item.url}" onerror="this.src='data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 100 100\'%3E%3Crect width=\'100\' height=\'100\' fill=\'%237627C5\'/%3E%3Ctext x=\'50\' y=\'55\' text-anchor=\'middle\' fill=\'white\' font-size=\'14\'%3E📸%3C/text%3E%3C/svg%3E'">
-            </div>
-        `;
-    }
-    grid.innerHTML = html;
-    
-    modal.style.display = 'flex';
-}
 
 function closeGalleryModal() {
     var modal = document.getElementById('galleryModal');
